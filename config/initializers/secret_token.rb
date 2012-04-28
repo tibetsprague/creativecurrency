1f95e20a991db99830d4423b29fb5eee8be739de
Tibet Sprague <tsprague@Tibetbook.local>
2012-04-27 17:21:30 -0700

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creativecurrency::Application.config.secret_token = '233b635034adc97492e2ffb1d23b3323e2f68191365f4dc8599ad2bb5e4fd93efc123d5dd9521809a0289a22320fa27152ef72162a76709bbc887d21efabcd3e'
